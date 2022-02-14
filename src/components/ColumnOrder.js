0e6b4d5c90b5abe076bfbf529ee7c0c5dcadeaa2
serdarsen <serdar.sen@kron.com.tr>
2022-02-14 21:25:54 +0300

    Add Column Order.

---
+++ src/components/ColumnOrder.js
@@ -0,0 +1,79 @@
+import React, { useMemo } from "react";
+import { useColumnOrder, useTable } from "react-table";
+import { COLUMNS } from "./columns";
+import MOCK_DATA from "./MOCK_DATA.json";
+import "./table.css";
+
+const ColumnOrder = () => {
+  const columnsMemo = useMemo(() => COLUMNS, []);
+  const dataMemo = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    setColumnOrder,
+  } = useTable(
+    {
+      columns: columnsMemo,
+      data: dataMemo,
+    },
+    useColumnOrder
+  );
+
+  const changeOrder = () => {
+    setColumnOrder([
+      "id",
+      "first_name",
+      "last_name",
+      "phone",
+      "country",
+      "date_of_birth",
+    ]);
+  };
+
+  return (
+    <>
+      <button onClick={changeOrder}>Change column order</button>
+      <table className="react-table" {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
+  );
+};
+
+export default ColumnOrder;
