57b83b9a1d976cd31dba4f782866a848859ebc42
serdarsen <serdar.sen@kron.com.tr>
2022-02-14 21:11:32 +0300

    Add Row Selection.

---
+++ src/components/RowSelection.js
@@ -0,0 +1,97 @@
+import React, { useMemo } from "react";
+import { useRowSelect, useTable } from "react-table";
+import Checkbox from "./Checkbox";
+import { COLUMNS } from "./columns";
+import MOCK_DATA from "./MOCK_DATA.json";
+import "./table.css";
+
+const RowSelection = () => {
+  const columnsMemo = useMemo(() => COLUMNS, []);
+  const dataMemo = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    selectedFlatRows,
+  } = useTable(
+    {
+      columns: columnsMemo,
+      data: dataMemo,
+    },
+    useRowSelect,
+    (hooks) => {
+      hooks.visibleColumns.push((columns) => {
+        return [
+          {
+            id: "selection",
+            Header: ({ getToggleAllRowsSelectedProps }) => (
+              <Checkbox {...getToggleAllRowsSelectedProps()} />
+            ),
+            Cell: ({ row }) => (
+              <Checkbox {...row.getToggleRowSelectedProps()} />
+            ),
+          },
+          ...columns,
+        ];
+      });
+    }
+  );
+
+  const firstPageRows = rows.slice(0, 10);
+
+  return (
+    <>
+      <table className="react-table" {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedFlatRows: selectedFlatRows.map((row) => row.original),
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+    </>
+  );
+};
+
+export default RowSelection;
